2aadb9ecee17cb38aa803adb06287c16e5ab194c
Ghaffaru <mudashiruagm@gmail.com>
2019-12-01 17:23:16 +0000

    few bugs

--- src/FeedbackServiceProvider.php
+++ src/FeedbackServiceProvider.php
@@ -30,11 +30,11 @@ class FeedbackServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__. '/views','feedback');
         $this->publishes([
-            __DIR__.'views' => resource_path('views/vendor/feedback'),
+            __DIR__.'views' => resource_path('/views/vendor/feedback'),
         ]);    
-        $this->publishes([
-            __DIR__.'/assets' => public_path('vendor/feedback'),
-        ], 'public');
+        // $this->publishes([
+        //     __DIR__.'/assets' => public_path('vendor/feedback'),
+        // ], 'public');
         $this->loadMigrationsFrom(__DIR__. '/database/migrations');
      
         $this->publishes([
